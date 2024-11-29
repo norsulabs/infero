2f967139b6362d0fa078f3f55026f7ae8c2d7d81
ameen-91 <mohammedameen9011@gmail.com>
2024-11-29 22:52:04 +0530

    added tabular lists

--- infero/main.py
+++ infero/main.py
@@ -1,13 +1,16 @@
 import os
 import subprocess
 import typer
-from infero.pull.download import check_model
+from infero.pull.download import check_model, pull_model
+from tabulate import tabulate
 from infero.convert.onnx import convert_to_onnx, convert_to_onnx_q8
 from infero.utils import (
     sanitize_model_name,
     get_models_dir,
     get_package_dir,
     print_neutral,
+    print_success_bold,
+    print_error,
 )
 from infero.pull.models import remove_model
 
@@ -15,12 +18,8 @@ app = typer.Typer(name="infero")
 
 
 @app.command("run")
-def pull(model: str, quantize: bool = False):
-    print
+def run(model: str, quantize: bool = False):
     if check_model(model):
-        convert_to_onnx(model)
-        if quantize:
-            convert_to_onnx_q8(model)
         model_path = os.path.join(get_models_dir(), sanitize_model_name(model))
         package_dir = get_package_dir()
         server_script_path = os.path.join(package_dir, "serve", "server.py")
@@ -31,14 +30,33 @@ def pull(model: str, quantize: bool = False):
         typer.echo("Failed to run model")
 
 
+@app.command("pull")
+def pull(model: str, quantize: bool = False):
+    if pull_model(model):
+        convert_to_onnx(model)
+        if quantize:
+            convert_to_onnx_q8(model)
+        print_success_bold(f"Model {model} pulled successfully")
+    else:
+        print_error("Failed to get model")
+
+
 @app.command("list")
 def list_models():
     if not os.path.exists(get_models_dir()):
         print_neutral("No models found")
         return
-    models = os.path.join(get_models_dir(), sanitize_model_name)
-    for model in os.listdir(models):
-        typer.echo(model)
+    models_dir = get_models_dir()
+    models = []
+    for model in os.listdir(models_dir):
+        quantized = (
+            "✅"
+            if os.path.exists(os.path.join(models_dir, model, "model_quantized.onnx"))
+            else ""
+        )
+        models.append([model, quantized])
+    table = tabulate(models, headers=["Name", "Quantized"], tablefmt="grid")
+    print_neutral(table)
 
 
 @app.command("remove")
--- infero/pull/models.py
+++ infero/pull/models.py
@@ -1,11 +1,9 @@
 import os
-from infero.utils import sanitize_model_name, print_success, print_error
+from infero.utils import sanitize_model_name, print_success, print_error, get_models_dir
 
 
 def remove_model(model):
-    model_path = os.path.join(
-        os.getcwd(), "infero/data/models", sanitize_model_name(model)
-    )
+    model_path = os.path.join(get_models_dir, sanitize_model_name(model))
     if os.path.exists(model_path):
         os.rmdir(model_path)
         print_success(f"Model {model} removed")
